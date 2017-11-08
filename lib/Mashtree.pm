672160053b079dc5b2e20adefaa14f219170d048
Lee Katz - Aspen <gzu2@cdc.gov>
2017-11-08 14:08:48 -0500

    v0.27

--- lib/Mashtree.pm
+++ lib/Mashtree.pm
@@ -25,7 +25,7 @@ local $0=basename $0;
 ######
 # CONSTANTS
 
-our $VERSION = "0.26";
+our $VERSION = "0.27";
 our $MASHTREE_VERSION=$VERSION;
 our @fastqExt=qw(.fastq.gz .fastq .fq .fq.gz);
 our @fastaExt=qw(.fasta .fna .faa .mfa .fas .fsa .fa);
