dc3b62820da19ea3a2fbff2fcbf0ec53f5dd6777
Lee Katz - Aspen <gzu2@cdc.gov>
2018-11-29 10:01:43 -0500

    v0.35.3 - added two more files to the manifest to make cpan happy

--- lib/Mashtree.pm
+++ lib/Mashtree.pm
@@ -25,7 +25,7 @@ local $0=basename $0;
 ######
 # CONSTANTS
 
-our $VERSION = "0.35.2";
+our $VERSION = "0.35.3";
 our $MASHTREE_VERSION=$VERSION;
 our @fastqExt=qw(.fastq.gz .fastq .fq .fq.gz);
 our @fastaExt=qw(.fasta .fna .faa .mfa .fas .fsa .fa);
