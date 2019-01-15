a93f42e4ee774f01ab213b547ffef946f736f5fd
Lee Katz - Aspen <gzu2@cdc.gov>
2019-01-15 16:43:57 -0500

    thoughts for replicates

--- bin/mashtree_wrapper.pl
+++ bin/mashtree_wrapper.pl
@@ -11,6 +11,8 @@ use File::Temp qw/tempdir tempfile/;
 use File::Basename qw/basename dirname fileparse/;
 use File::Copy qw/cp mv/;
 use List::Util qw/shuffle/;
+use List::MoreUtils qw/part/;
+use POSIX qw/floor/;
 
 use Fcntl qw/:flock LOCK_EX/;
 
@@ -28,9 +30,9 @@ use Bio::Tree::DistanceFactory;
 use Bio::Tree::Statistics;
 use Bio::Matrix::IO;
 
+my $writeStick :shared;
+
 local $0=basename $0;
-my $writeStick :shared;  # Only one thread can write at a time
-my $gzipStick  :shared;  # Only one thread can write at a time
 
 exit main();
 
@@ -70,20 +72,49 @@ sub main{
     die "ERROR: outmatrix was specified for mashtree but should be an option for $0";
   }
   
+  # Separate flagged options from reads in the mashtree options
+  my @reads = ();
+  my @mashOptions = ();
+  for(my $i=0;$i<@ARGV;$i++){
+    if(-e $ARGV[$i]){
+      push(@reads, $ARGV[$i]);
+    } else {
+      push(@mashOptions, $ARGV[$i]);
+    }
+  }
+
   # Copy reads over to the temp storage where I assume it is faster
   # and where we can write .lock files.
   my $inputdir = "$$settings{tempdir}/input";
   mkdir $inputdir;
-  for(my $i=0;$i<@ARGV;$i++){
-    if(-e $ARGV[$i]){
-      logmsg "Copying $ARGV[$i] to temp space - $inputdir";
-      my $copiedFile = "$inputdir/".basename($ARGV[$i]);
-      cp($ARGV[$i], $copiedFile);
-      $ARGV[$i] = $copiedFile;
-    }
+  my $tmp_i= 0;
+  my @reads_per_thread = ([@reads]);
+  if($$settings{numcpus} > 1){
+    @reads_per_thread = part { $tmp_i++ % ($$settings{numcpus}-1) } @reads;
   }
+  my @cpThread;
+  for(0..$$settings{numcpus}-1){
+    $cpThread[$_] = threads->new(sub{
+      my($fileArr) = @_;
+      my @copiedReads;
+      for my $file(@$fileArr){
+        logmsg "Copying $file to temp space - $inputdir";
+        my $copiedFile = "$inputdir/".basename($file);
+        cp($file, $copiedFile);
+        push(@copiedReads, $copiedFile);
+      }
+      return \@copiedReads;
+    }, $reads_per_thread[$_]);
+  }
+  @reads = ();
+  for(@cpThread){
+    my $tmp = $_->join;
+    push(@reads, @$tmp);
+  }
+  logmsg "Finished copying input data to $inputdir";
 
-  my $mashOptions=join(" ",@ARGV);
+  my $mashOptions=join(" ",@mashOptions);
+  my $reads = join(" ", @reads);
   
   # Some filenames we'll expect
   my $observeddir="$$settings{tempdir}/observed";
@@ -91,19 +122,13 @@ sub main{
   my $observedTree="$$settings{tempdir}/observed.dnd";
   my $outmatrix="$$settings{tempdir}/observeddistances.tsv";
 
-  # Make the observed directory and run Mash
-  mkdir($observeddir);
-  system("$FindBin::RealBin/mashtree --outmatrix $outmatrix.tmp --tempdir $observeddir --numcpus $$settings{numcpus} $mashOptions > $observedTree.tmp");
-  die if $?;
-  mv("$observedTree.tmp",$observedTree) or die $?;
-  mv("$outmatrix.tmp",$outmatrix) or die $?;
-
   # Multithreaded reps
-  my $repQueue=Thread::Queue->new(1..$$settings{reps});
+  my @rep_id = (1..$$settings{reps});
+  my $repsPerThread = int($$settings{reps} / $$settings{numcpus}) + 1;
   my @thr;
   for(0..$$settings{numcpus}-1){
-    $thr[$_]=threads->new(\&repWorker, $mashOptions, $repQueue, $settings);
-    $repQueue->enqueue(undef);
+    my @theseReps = splice(@rep_id, 0, $repsPerThread);
+    $thr[$_]=threads->new(\&repWorker, \@mashOptions, \@reads, \@theseReps, $settings);
   }
 
   my @bsTree;
@@ -114,6 +139,14 @@ sub main{
     }
   }
   
+  # Make the observed directory and run Mash
+  logmsg "Running mashtree on full data";
+  mkdir($observeddir);
+  system("$FindBin::RealBin/mashtree --outmatrix $outmatrix.tmp --tempdir $observeddir --numcpus $$settings{numcpus} $mashOptions $reads > $observedTree.tmp");
+  die if $?;
+  mv("$observedTree.tmp",$observedTree) or die $?;
+  mv("$outmatrix.tmp",$outmatrix) or die $?;
+
   # Combine trees into a bootstrapped tree and write it 
   # to an output file. Then print it to stdout.
   logmsg "Adding bootstraps to tree";
@@ -140,55 +173,73 @@ sub main{
 }
 
 sub repWorker{
-  my($mashOptions,$repQueue,$settings)=@_;
-
-  my @argv = split(/\s+/, $mashOptions);
-
+  my($mashOptions, $reads, $reps,$settings)=@_;
   my @bsTree;
-  while(defined(my $rep=$repQueue->dequeue())){
+  if(!defined($reps) || ref($reps) ne 'ARRAY' || !@$reps){
+    return \@bsTree;
+  }
+
+  my @mashOptions = @$mashOptions;
+  my @reads       = @$reads;
+
+  my $numcpus = floor($$settings{numcpus}/$$settings{reps});
+  $numcpus = 1 if($numcpus < 1);
+
+  for my $rep(@$reps){
     my $repTempdir="$$settings{tempdir}/rep$rep";
     mkdir $repTempdir;
     logmsg "Starting mashtree replicate $rep - $repTempdir";
     
-    my @opts;
     #logmsg "Downsampling reads (replicate $rep).";
     # Downsample the reads
-    for my $argv(@argv){
-      if(! -e $argv){
-        push(@opts, $argv);
-        next;
-      }
+    my @downsampledReads=();
+    for my $r(@reads){
+      my $newReads = "$repTempdir/".basename($r);
+      #logmsg "DEBUG";push(@downsampledReads, $newReads);next;
 
-      my $newReads = "$repTempdir/".basename($argv);
       my @buffer = ();
-      open(my $lockFh, ">", "$argv.lock") or die "ERROR: could not make lockfile $argv.lock: $!";
-      flock($lockFh, LOCK_EX) or die "ERROR locking file $argv.lock: $!";
+      open(my $lockFh, ">", "$r.lock") or die "ERROR: could not make lockfile $r.lock: $!";
+      flock($lockFh, LOCK_EX) or die "ERROR locking file $r.lock: $!";
 
-      open(my $inFh, "zcat $argv | ") or die "ERROR reading $argv for downsampling: $!";
+      open(my $inFh, "zcat $r | ") or die "ERROR reading $r for downsampling: $!";
       open(my $outFh," | gzip -c > $newReads") or die "ERROR gzipping to $newReads: $!";
+      close $outFh;
       while(my $id=<$inFh>){
         my $seq =<$inFh>;
         my $plus=<$inFh>;
         my $qual=<$inFh>;
         if(rand(1) < 0.5){
           push(@buffer, $id.$seq.$plus.$qual);
-          if(scalar(@buffer) > 10000){
-            print $outFh join("",@buffer);
+          # The buffer size is something like 600 bytes per entry
+          # times 100,000 entries times 12 threads = 720Mb.
+          if(scalar(@buffer) > 100000){
+            {
+              # Only let one thread at a time write to the file but
+              # this makes us open the file in append mode.
+              lock($writeStick);
+              open($outFh, " | gzip -c >> $newReads") or die "ERROR gzipping to $newReads: $!";
+              print $outFh join("",@buffer);
+              close $outFh;
+            }
             @buffer = (); # flush the buffer
           }
         }
       } 
       # Finish the remaining entries in the buffer
-      print $outFh join("",@buffer);
-      close $outFh;
+      {
+        lock($writeStick);
+        open($outFh, " | gzip -c >> $newReads") or die "ERROR gzipping to $newReads: $!";
+        print $outFh join("",@buffer);
+        close $outFh;
+      }
       close $inFh;
       close $lockFh;
 
-      push(@opts, $newReads);
+      push(@downsampledReads, $newReads);
     }
 
     logmsg "Done downsampling for replicate $rep. Running mashtree on files in $repTempdir";
-    my $log = `mashtree --numcpus 1 @opts 2>&1 > $repTempdir/tree.dnd`;
+    my $log = `mashtree --numcpus $numcpus @mashOptions @downsampledReads 2>&1 > $repTempdir/tree.dnd`;
     if($?){
       die "ERROR with mashtree on rep $rep (exit code $?):\n$log";
     }
